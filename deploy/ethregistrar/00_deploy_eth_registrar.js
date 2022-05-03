de7ac74a8794d311cda8dd222e535a80ead90f02
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-05-03 17:29:56 +0530

    Adding reverse reverse registrar address as an argument

--- deploy/ethregistrar/00_deploy_eth_registrar.js
+++ deploy/ethregistrar/00_deploy_eth_registrar.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 
 module.exports = async ({getNamedAccounts, deployments, network}) => {
@@ -7,15 +8,17 @@ module.exports = async ({getNamedAccounts, deployments, network}) => {
 
     const baseRegistrar = await ethers.getContract('BaseRegistrarImplementation');
 
-    const priceOracle = await ethers.getContract('StablePriceOracle')
+    const priceOracle = await ethers.getContract('StablePriceOracle');
+
+    const reverseRegistrar = await ethers.getContract('ReverseRegistrar')
 
     await deploy('ETHRegistrarController', {
         from: deployer, 
-        args: [baseRegistrar.address, priceOracle.address, 600, 86400],
+        args: [baseRegistrar.address, priceOracle.address, 600, 86400, reverseRegistrar.address, ZERO_ADDRESS],
         log: true
     })  
 
 }
 
 module.exports.tags = ['eth-registrar'];
-module.exports.dependencies = ['registry', 'oracles']
+module.exports.dependencies = ['registry', 'oracles', 'reverse-registrar']
