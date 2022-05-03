b8cd7c99360cd469d15c97293b8d5203c6e57868
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-05-03 16:14:31 +0530

    Removing transactions from public resolver

--- deploy/registry/10_deploy_public_resolver.js
+++ deploy/registry/10_deploy_public_resolver.js
@@ -1,8 +1,5 @@
 const { ethers } = require("hardhat");
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-const ZERO_HASH = "0x0000000000000000000000000000000000000000000000000000000000000000"
-const namehash = require('eth-ens-namehash');
-const sha3 = require('web3-utils').sha3;
 
 module.exports = async ({getNamedAccounts, deployments, network}) => {
     const {deploy} = deployments;
@@ -16,14 +13,6 @@ module.exports = async ({getNamedAccounts, deployments, network}) => {
         log: true
     })
 
-    const resolver = await ethers.getContract('PublicResolver')
-
-    const transactions = []
-    transactions.push(await ens.setSubnodeOwner(ZERO_HASH, sha3('avax'), deployer))
-    transactions.push(await ens.setResolver(namehash.hash('avax'), resolver.address))
-    transactions.push(await resolver['setAddr(bytes32,address)'](namehash.hash('avax'), resolver.address))
-    console.log(`Waiting on settings to take place on resolvers ${transactions.length}`)
-    await Promise.all(transactions.map((tx) => tx.wait()));
     
     // const rootOwner = await ens.owner(ZERO_HASH);
     //     switch(rootOwner) {
