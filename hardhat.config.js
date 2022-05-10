2252f2f49433a00d4a2d5c3fbcffa829db7868cb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-05-10 12:17:51 +0530

    Updating hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -57,7 +57,7 @@ module.exports = {
     },
     localhost: {
       url: 'http://127.0.0.1:8545',
-      saveDeployments: true,
+      saveDeployments: false,
       tags: ['test', 'legacy', 'use_root'],
     },
     ropsten: {
